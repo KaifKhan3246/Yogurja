c87e5a4a11ad806138dd5d98ba6cf1195b407d7b
KaifKhan3246 <jatukaif0099@gmail.com>
2025-12-14 16:04:22 +0530

    Update Cancel.jsx

--- src/components/Cancel.jsx
+++ src/components/Cancel.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Cancel = () => {
   return (
-    <div>
-      <h1>cancel</h1>
+    <div className='h-full'>
+      <div className='bg-black text-white flex flex-col flex-wrap justify-center items-center h-40 md:h-50 font-semibold font-serif'>
+        <h1 className='md:text-6xl text-4xl'>Terms and Conditions</h1>
+        <span className='text-sm md:text-lg'>Home/Terms and Conditions</span>
+      </div>
     </div>
   )
 }
